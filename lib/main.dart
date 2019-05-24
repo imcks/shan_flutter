8e084ecdbec21b77e3459462ad2ddab4e38473d7
shan <shan@shandeMacBook-Pro.local>
2019-05-24 22:23:33 +0800

    自定义图标小部件

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import './demo/listview_demo.dart';
 import './demo/drawer_demo.dart';
 import './demo/bottom_navigation_bar_demo.dart';
 import './demo/basic_demo.dart';
+import './demo/layout_demo.dart';
  
  void main() => runApp(App());
 
@@ -67,7 +68,8 @@ import './demo/basic_demo.dart';
             ListViewDemo(),
             // Text('这是第二个页面'),
             BasicDemo(),
-            Icon(Icons.directions_bike,size: 128,color: Colors.black12),
+            // Icon(Icons.directions_bike,size: 128,color: Colors.black12),
+            LayoutDemo(),
           ],
         ),
         drawer: DrawerDemo(),
