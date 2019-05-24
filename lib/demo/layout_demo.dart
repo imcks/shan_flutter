e1f70b95484ea11c8d2186b376e3aebd6548f872
shan <shan@shandeMacBook-Pro.local>
2019-05-24 22:28:30 +0800

    图标水平排列

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        IconBadge(Icons.pool),
-      ],
+    return Container(
+      child: Row(
+        children: <Widget>[
+          IconBadge(Icons.pool),
+          IconBadge(Icons.beach_access),
+          IconBadge(Icons.airplanemode_active),
+        ],
+      ),
     );
   }
 }
@@ -15,18 +19,15 @@ class IconBadge extends StatelessWidget {
   final IconData icon;
   final double size;
 
-  IconBadge(this.icon, {
-    this.size = 32.0
-  });
+  IconBadge(this.icon, {this.size = 32.0});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Icon(icon,size: size,color: Colors.white),
+      child: Icon(icon, size: size, color: Colors.white),
       width: size + 60,
       height: size + 60,
       color: Color.fromRGBO(3, 54, 255, 1.0),
     );
   }
-
 }
